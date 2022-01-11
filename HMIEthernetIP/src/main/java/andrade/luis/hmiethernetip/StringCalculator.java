7b39126ef99ff9d93bd1419e0b6264f1a989db95
andr3slelouch <luis.andradec14@gmail.com>
2022-01-11 12:22:47 -0500

    Environment prepared for development

---
+++ HMIEthernetIP/src/main/java/andrade/luis/hmiethernetip/StringCalculator.java
@@ -0,0 +1,7 @@
+package andrade.luis.hmiethernetip;
+
+public class StringCalculator {
+    public int add(int a, int b){
+        return 0;
+    }
+}
---
+++ HMIEthernetIP/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module andrade.luis.hmiethernetip {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens andrade.luis.hmiethernetip to javafx.fxml;
+    exports andrade.luis.hmiethernetip;
+}
